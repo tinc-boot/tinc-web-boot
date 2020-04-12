08044cf9a194ecbcbc767e2d916ba680accca91c
Alexander Baryshnikov <dev@baryshnikov.net>
2020-04-12 22:58:49 +0800

    use ui as submodule

--- network/event_types.go
+++ network/event_types.go
@@ -1,6 +1,6 @@
 package network
 
-//go:generate events-gen -p network -E Events -s -P -o events.go --ts ../web/ui/src/api/events.ts
+//go:generate events-gen -p network -E Events -s -P -o events.go --ts ../events.ts
 
 //event:"Started"
 //event:"Stopped"
