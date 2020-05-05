de5104b94d81a4d0a3277ab8ebee2aa668aa9bf6
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-05 19:18:43 +0800

    support configuration from file

--- cmd/tinc-web-boot/internal/server.go
+++ cmd/tinc-web-boot/internal/server.go
@@ -8,11 +8,11 @@ import (
 )
 
 type HttpServer struct {
-	GracefulShutdown time.Duration `name:"graceful-shutdown" env:"GRACEFUL_SHUTDOWN" help:"Interval before server shutdown" default:"15s"`
-	Bind             string        `name:"bind" env:"BIND" help:"Address to where bind HTTP server" default:"127.0.0.1:8686"`
-	TLS              bool          `name:"tls" env:"TLS" help:"Enable HTTPS serving with TLS"`
-	CertFile         string        `name:"cert-file" env:"CERT_FILE" help:"Path to certificate for TLS" default:"server.crt"`
-	KeyFile          string        `name:"key-file" env:"KEY_FILE" help:"Path to private key for TLS" default:"server.key"`
+	GracefulShutdown time.Duration `name:"graceful-shutdown" env:"GRACEFUL_SHUTDOWN" help:"Interval before server shutdown" default:"15s" json:"graceful_shutdown"`
+	Bind             string        `name:"bind" env:"BIND" help:"Address to where bind HTTP server" default:"127.0.0.1:8686" json:"bind"`
+	TLS              bool          `name:"tls" env:"TLS" help:"Enable HTTPS serving with TLS" json:"tls"`
+	CertFile         string        `name:"cert-file" env:"CERT_FILE" help:"Path to certificate for TLS" default:"server.crt" json:"crt_file"`
+	KeyFile          string        `name:"key-file" env:"KEY_FILE" help:"Path to private key for TLS" default:"server.key" json:"key_file"`
 }
 
 func (qs *HttpServer) Serve(globalCtx context.Context, handler http.Handler) error {
